32b63df70dbce50e4f9f6c121ead6b94b80debe2
ORIGINAL SHOP <arkonteoumnia@gmail.com>
2023-03-25 16:17:52 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecapitulatifCreateComponent } from './view/recapitulatifs/recapitulatif-create/recapitulatif-create.component';
+import { RecapitulatifListComponent } from './view/recapitulatifs/recapitulatif-list/recapitulatif-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecapitulatifCreateComponent,
+    RecapitulatifListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
